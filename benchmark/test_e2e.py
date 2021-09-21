2bad6ba595b4d30317e343e29a6af1b5cce2ab95
jaxa1337 <jakub21321@gmail.com>
2021-09-21 15:25:17 +0200

    Fix tests

--- benchmark/test_e2e.py
+++ benchmark/test_e2e.py
@@ -7,6 +7,7 @@ import signal
 import sys
 from scapy.all import *
 from influxdb import InfluxDBClient
+from int_package_generator import *
 
 sys.path.insert(0, './benchmark')
 from INTReport import TelemetryReport_v10, INT_v10
@@ -14,9 +15,9 @@ from INTReport import TelemetryReport_v10, INT_v10
 inif = "int_veth_0"
 outif = "int_veth_1"
 subp = None
-test_db = "INT_test_database"
+test_db = "int_test_database"
 idbclient = InfluxDBClient(host="localhost", database=test_db)
-python_ver = "python3" if sys.version_info[0] == 3 else "python2"
+python_ver = "python3"
 
 def start_collector(cmd):
     global subp
@@ -27,10 +28,10 @@ def start_collector(cmd):
         print(e)
         return None
 
-    p = subp.stdout.readline()
+    # p = subp.stdout.readline()
     assert (subp.poll() == None), "Error loading XDP program"
 
-    time.sleep(0.5)
+    time.sleep(3)
 
 @pytest.fixture
 def setup_veth():
@@ -51,108 +52,77 @@ def end_to_end_influxdb_v10(cmd):
     start_collector(cmd)
     assert (subp != None), "Fail subprocess to load XDP program"
 
+    int_length = 3 * 8 + 3
+    packets = []
+    int_metadata = INTMetadata(3)
+    int_metadata.create_metadata()
+
     p0 = Ether()/ \
             IP(tos=0x17<<2)/ \
-            UDP(sport=5000, dport=8086)/ \
-            TelemetryReport_v10(ingressTimestamp= 1524138290)/ \
+            UDP(sport=5000, dport=8090)/ \
+            TelemetryReport_v10(swid = 1, seqNumber = 5, ingressTimestamp= 1524138290)/ \
             Ether()/ \
             IP(src="10.0.0.1", dst="10.0.0.2")/ \
             UDP(sport=5000, dport=5000)/ \
-            INT_v10(length=27, hopMLen=8, remainHopCnt=3, ins=(1<<7|1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1)<<8,
-                INTMetadata= [4, 5<<16| 3, 400, 5<<16| 600, 700, 1524234560, 5<<16| 1000, 1,
-                5, 2<<16| 3, 4, 5<<16| 6, 7, 1524234561, 5<<16| 10, 1,
-                6, 2<<16| 3, 4, 5<<16| 6, 7, 1524234562, 5<<16| 10, 1]
-            )
+            INT_v10(length=int_length,hopMLen=8, remainHopCnt=3, ins=(1<<7|1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1)<<8,
+                INTMetadata= int_metadata.int_metadata)
+
+    int_metadata.edit_hop_latency(70)
+    int_metadata.edit_queue_occups()
+    int_metadata.edit_timestamps()
+    int_metadata.edit_tx_utilizes(3)
 
     p1 = Ether()/ \
-            IP(tos=0x17<<2)/ \
-            UDP(sport=5000, dport=8086)/ \
-            TelemetryReport_v10(ingressTimestamp= 1524138290)/ \
-            Ether()/ \
-            IP(src="10.0.0.1", dst="10.0.0.2")/ \
-            UDP(sport=5000, dport=5000)/ \
-            INT_v10(length=27, hopMLen=8, remainHopCnt=3, ins=(1<<7|1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1)<<8,
-                INTMetadata= [4, 5<<16| 3, 400, 5<<16| 600, 700, 1524234561, 5<<16| 1000, 1,
-                5, 2<<16| 3, 4, 5<<16| 6, 7, 1524234562, 5<<16| 10, 1,
-                6, 2<<16| 3, 4, 5<<16| 6, 7, 1524234563, 5<<16| 10, 1]
-            )
-
-    p2 = Ether()/ \
-            IP(tos=0x17<<2)/ \
-            UDP(sport=5000, dport=8086)/ \
-            TelemetryReport_v10(ingressTimestamp= 1524138300)/ \
-            Ether()/ \
-            IP(src="10.0.0.1", dst="10.0.0.2")/ \
-            UDP(sport=5000, dport=5000)/ \
-            INT_v10(length=27, hopMLen=8, remainHopCnt=3, ins=(1<<7|1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1)<<8,
-                INTMetadata= [4, 5<<16| 3, 4, 5<<16| 6, 7, 1524234570, 5<<16| 10, 1000,
-                5, 2<<16| 3, 4, 5<<16| 6, 7, 1524234571, 5<<16| 10, 1,
-                6, 2<<16| 3, 4, 5<<16| 6, 7, 1524234572, 5<<16| 10, 1]
-            )
+        IP(tos=0x17<<2)/ \
+        UDP(sport=5000, dport=8090)/ \
+        TelemetryReport_v10(swid = 1,seqNumber = 200,ingressTimestamp= 1524138290)/ \
+        Ether()/ \
+        IP(src="10.0.0.1", dst="10.0.0.2")/ \
+        UDP(sport=5000, dport=5000)/ \
+        INT_v10(length=int_length,hopMLen=8, remainHopCnt=3, ins=(1<<7|1<<6|1<<5|1<<4|1<<3|1<<2|1<<1|1)<<8,
+            INTMetadata= int_metadata.int_metadata)
 
     for p in [p0, p1]:
-        # the value should keep same as p0
+        
         sendp(p0, iface=inif)
         time.sleep(1)
-        assert len(idbclient.get_list_measurements()) == 10
+        
+        db_flag = False
+        for db in idbclient.get_list_database():
+            if test_db == db['name']:
+                db_flag = True   
+        assert db_flag == True
+        time.sleep(1)
 
-        r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=4\"", epoch='ns')
-        assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=4')) == [{u'value': 400, u'time': 1524234560}]
-        r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=5\"", epoch='ns')
-        assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=5')) == [{u'value': 4, u'time': 1524234561}]
-        r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=6\"", epoch='ns')
-        assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=6')) == [{u'value': 4, u'time': 1524234562}]
+        measurements_flag = False
+        for measurement in idbclient.get_list_measurements():
+            if measurement['name'] == 'int_telemetry_new_collector':
+                measurements_flag = True
+        assert measurements_flag == True
 
-        r = idbclient.query("select * from \"port_tx_utilize,sw_id=4,port_id=3\"", epoch='ns')
-        assert list(r.get_points('port_tx_utilize,sw_id=4,port_id=3')) == [{u'value': 1, u'time': 1524234560}]
-        r = idbclient.query("select * from \"port_tx_utilize,sw_id=5,port_id=3\"", epoch='ns')
-        assert list(r.get_points('port_tx_utilize,sw_id=5,port_id=3')) == [{u'value': 1, u'time': 1524234561}]
-        r = idbclient.query("select * from \"port_tx_utilize,sw_id=6,port_id=3\"", epoch='ns')
-        assert list(r.get_points('port_tx_utilize,sw_id=6,port_id=3')) == [{u'value': 1, u'time': 1524234562}]
+        r = idbclient.query('select * from int_telemetry_new_collector', epoch='ns')
+        e2e_report = list(r.get_points(measurement = 'int_telemetry_new_collector', 
+                                     tags={"seq": 5}))[0]
+        assert (e2e_report['origts']==700 and e2e_report['dstts']==700)
 
-        r = idbclient.query("select * from \"queue_occupancy,sw_id=4,queue_id=5\"", epoch='ns')
-        assert list(r.get_points('queue_occupancy,sw_id=4,queue_id=5')) == [{u'value': 600, u'time': 1524234560}]
-        r = idbclient.query("select * from \"queue_occupancy,sw_id=5,queue_id=5\"", epoch='ns')
-        assert list(r.get_points('queue_occupancy,sw_id=5,queue_id=5')) == [{u'value': 6, u'time': 1524234561}]
-        r = idbclient.query("select * from \"queue_occupancy,sw_id=6,queue_id=5\"", epoch='ns')
-        assert list(r.get_points('queue_occupancy,sw_id=6,queue_id=5')) == [{u'value': 6, u'time': 1524234562}]
+        hop_index_0 = list(r.get_points(measurement = 'int_telemetry_new_collector',
+                                        tags = {'hop_index': '0'}))[0]
+        assert hop_index_0['hop_delay']==20
 
-        r = idbclient.query("select * from \"flow_stat,10.0.0.1:5000->10.0.0.2:5000,proto=17\"", epoch='ns')
-        assert list(r.get_points('flow_stat,10.0.0.1:5000->10.0.0.2:5000,proto=17')) == [{u'path': u'6:5:4', u'flow_latency': 408, u'time': 1524138290}]
+        hop_index_1 = list(r.get_points(measurement = 'int_telemetry_new_collector',
+                                        tags = {'hop_index': '1'}))[0]
+        assert hop_index_1['hop_delay']==20
 
+        hop_index_2 = list(r.get_points(measurement = 'int_telemetry_new_collector',
+                                        tags = {'hop_index': '2'}))[0]
+        assert hop_index_2['hop_delay']==20
 
-    sendp(p2, iface=inif)
-    time.sleep(1)
-    assert len(idbclient.get_list_measurements()) == 10
-    r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=4\"", epoch='ns')
-    assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=4')) == [{u'value': 400, u'time': 1524234560}, {u'value': 4, u'time': 1524234570}]
-    r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=5\"", epoch='ns')
-    assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=5')) == [{u'value': 4, u'time': 1524234561}]
-    r = idbclient.query("select * from \"flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=6\"", epoch='ns')
-    assert list(r.get_points('flow_hop_latency,10.0.0.1:5000->10.0.0.2:5000,proto=17,sw_id=6')) == [{u'value': 4, u'time': 1524234562}]
-
-    r = idbclient.query("select * from \"port_tx_utilize,sw_id=4,port_id=3\"", epoch='ns')
-    assert list(r.get_points('port_tx_utilize,sw_id=4,port_id=3')) == [{u'value': 1, u'time': 1524234560}, {u'value': 1000, u'time': 1524234570}]
-    r = idbclient.query("select * from \"port_tx_utilize,sw_id=5,port_id=3\"", epoch='ns')
-    assert list(r.get_points('port_tx_utilize,sw_id=5,port_id=3')) == [{u'value': 1, u'time': 1524234561}]
-    r = idbclient.query("select * from \"port_tx_utilize,sw_id=6,port_id=3\"", epoch='ns')
-    assert list(r.get_points('port_tx_utilize,sw_id=6,port_id=3')) == [{u'value': 1, u'time': 1524234562}]
-
-    r = idbclient.query("select * from \"queue_occupancy,sw_id=4,queue_id=5\"", epoch='ns')
-    assert list(r.get_points('queue_occupancy,sw_id=4,queue_id=5')) == [{u'value': 600, u'time': 1524234560}, {u'value': 6, u'time': 1524234570}]
-    r = idbclient.query("select * from \"queue_occupancy,sw_id=5,queue_id=5\"", epoch='ns')
-    assert list(r.get_points('queue_occupancy,sw_id=5,queue_id=5')) == [{u'value': 6, u'time': 1524234561}]
-    r = idbclient.query("select * from \"queue_occupancy,sw_id=6,queue_id=5\"", epoch='ns')
-    assert list(r.get_points('queue_occupancy,sw_id=6,queue_id=5')) == [{u'value': 6, u'time': 1524234562}]
-
-    r = idbclient.query("select * from \"flow_stat,10.0.0.1:5000->10.0.0.2:5000,proto=17\"", epoch='ns')
-    assert list(r.get_points('flow_stat,10.0.0.1:5000->10.0.0.2:5000,proto=17')) == [{u'path': u'6:5:4', u'flow_latency': 408, u'time': 1524138290}, {u'path': u'6:5:4', u'flow_latency': 12, u'time': 1524138300}]
-
+        # print(e2e_report,'\n')
+        # print(hop_index_0,'\n')
+        # print(hop_index_1,'\n')
+        # print(hop_index_2,'\n')
+        # assert False
 
 def test_e2e_indb_threshold_v10(setup_veth):
-    cmd = [python_ver, "InDBClient.py", "-t", "-D", test_db, outif]
-    end_to_end_influxdb_v10(cmd)
-
-def test_e2e_indb_interval_v10(setup_veth):
-    cmd = [python_ver, "InDBClient.py", "-t", "-e", "INTERVAL", "-D", test_db, outif]
+    cmd = ['python3', "collector/InDBClient.py", outif, '-D', test_db]
     end_to_end_influxdb_v10(cmd)
